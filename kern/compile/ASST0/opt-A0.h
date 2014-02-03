243ab409a0461340c4e368592acbce7cdd4bfb67
Nick Stephens <nick.stephens93@gmail.com>
2014-02-03 00:16:22 -0800

    Remove object files and compile directory from repo

--- kern/compile/ASST0/opt-A0.h
+++
@@ -1,5 +0,0 @@
-/* Automatically generated; do not edit */
-#ifndef _OPT_A0_H_
-#define _OPT_A0_H_
-#define OPT_A0 1
-#endif /* _OPT_A0_H_ */
--- kern/compile/ASST0/opt-A4.h
+++
@@ -1,5 +0,0 @@
-/* Automatically generated; do not edit */
-#ifndef _OPT_A4_H_
-#define _OPT_A4_H_
-#define OPT_A4 0
-#endif /* _OPT_A4_H_ */
