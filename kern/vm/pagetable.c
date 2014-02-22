c3e91b9fa6146725652f7d23ed69fe7a6736e99e
Nick Stephens <nick.stephens93@gmail.com>
2014-02-21 21:56:47 -0800

    Implement pagetable API functions

--- kern/vm/pagetable.c
+++ kern/vm/pagetable.c
@@ -2,11 +2,14 @@
 #include <lib.h>
 #include <synch.h>
 #include <machine/vm.h>
+#include <thread.h>
+#include <curthread.h>
 #include <pagetable.h>
 
 void
 pagetable_bootstrap(void)
 {
+	int i;
 	u_int32_t lo, hi;
 	u_int32_t total;
 	u_int32_t frames;
@@ -33,7 +36,7 @@ pagetable_bootstrap(void)
 	kprintf("pagetable_bootstrap: pagetable initialing with:\n");
 	kprintf("available number of PTEs per frame: %d\n", pteposs*pframes);
 	kprintf("number of frames occupied by pagetable: %d\n", pframes);
-	kprintf("number of frames available: %d\n", pframes);
+	kprintf("number of frames available: %d\n", frames);
 
 	/* if this happens our while loop went wild */
 	assert(frames!=0);
@@ -42,8 +45,133 @@ pagetable_bootstrap(void)
 	pagetable = (struct pte *) PADDR_TO_KVADDR(ram_stealmem(pframes));
 	kprintf("pagetable initialized at 0x%08x\n", pagetable);
 
+	bframe = lo + PAGE_SIZE * pframes;
+	kprintf("bottom frame: 0x%08x\n", bframe);
+
+	pagetable_size = frames;
+
+	/* invalidate all the pagetable entries */
+	for(i=0;((u_int32_t) i)<pagetable_size;i++)
+		pagetable[i].control = 0;
+
 	pagetable_lock = lock_create("pagetable_lock");
 	if (pagetable_lock==NULL)
 		panic("pagetable_bootstrap: unable to initialize pagetable_lock\n");
 
+	pagetable_initialized = 1;
+
+}
+
+/* define alloc_kpages (malloc) here for the time being */
+/* kernel pages are a special case. since they will never 
+ * be asked to be resolve by mips there only real presence
+ * is to tell userland that a frame is occupied */
+vaddr_t
+alloc_kpages(int npages)
+{
+	u_int32_t i;
+	paddr_t free;
+	int j;
+
+	if (pagetable_initialized)
+	{
+		free = 0;
+		for(i=0;i<pagetable_size;i++)
+		{
+			kprintf("page [%d] validity: %d\n", i, PTE_VALID(pagetable[i])); 
+			if (!PTE_VALID(pagetable[i]))
+			{
+				for(j=1;j<npages;j++)
+				{
+					if (PTE_VALID(pagetable[i+j]))
+						break;
+				}
+				if (j==npages)
+				{
+					pagetable[i].control = VALID_B | SUPER_B;
+					free = FRAME(i);
+					break;
+				}
+			}
+		}
+	}
+	else
+	{
+		free = ram_stealmem(npages);
+	}
+
+	return PADDR_TO_KVADDR(free);
+}
+
+/* for now we just invalidate one page. This won't work if more than one
+ * page was originally allocated */
+void
+free_kpages(vaddr_t page)
+{
+	if (pagetable_initialized)
+		invalidatepage(page);
+}
+
+void
+addpage(vaddr_t page, int read, int write, int execute)
+{
+	int index; 
+	struct pte *pre, *cur;
+
+	index = hash(page);
+
+	lock_acquire(pagetable_lock);
+	pre = NULL;
+	cur = &pagetable[index];
+
+	/* XXX possible this loops forever */
+	while (cur->control & VALID_B)
+	{
+		pre = cur;
+		if (cur->next==-1)
+			index = (index*index) % pagetable_size;
+		else
+			index = cur->next;
+		cur = &pagetable[index];
+	}
+
+	if (pre!=NULL)
+		pre->next = index;
+
+	cur->page    = page;
+	cur->owner   = curthread->t_pid;
+	cur->control = 0;
+	
+	if (read)	
+		cur->control |= R_B;
+	if (write)
+		cur->control |= W_B;
+	if (execute)
+		cur->control |= X_B;
+
+	cur->control |= VALID_B;
+	cur->next = -1;
+
+	lock_release(pagetable_lock);
+}
+
+/* invalidates the page pointed to by page. Resolves the index to invalidate 
+ * by hashing both the page and pid */
+void
+invalidatepage(vaddr_t page)
+{
+	lock_acquire(pagetable_lock);
+	pagetable[hash(page)].control &= ~VALID_B;
+	lock_release(pagetable_lock);
+}
+
+/* a stupid hash function for the inverted page table */
+/* for the most part a lot of these values are somewhat arbitrary until
+ * I take the time to research what makes a good hash function. Generally
+ * speaking we want to give more weight to the pid because these
+ * are guaranteed to be unique */
+int
+hash(vaddr_t page)
+{
+	return ((page/10) + ((curthread->t_pid * 7) * 0x01000000)) % pagetable_size;
 }
